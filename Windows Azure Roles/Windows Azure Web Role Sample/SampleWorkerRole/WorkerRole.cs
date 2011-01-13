6c6b33a8d8e26baf107173cacfb8cd1e9e5b7974
Adron Hall <adronhall@gmail.com>
2011-01-12 18:59:26 -0800

    Added more changes to the sample role application.

--- Windows Azure Roles/Windows Azure Web Role Sample/SampleWorkerRole/WorkerRole.cs
+++ Windows Azure Roles/Windows Azure Web Role Sample/SampleWorkerRole/WorkerRole.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+﻿using System.Diagnostics;
 using System.Net;
 using System.Threading;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
 using Microsoft.WindowsAzure.ServiceRuntime;
-using Microsoft.WindowsAzure.StorageClient;
 
 namespace SampleWorkerRole
 {
@@ -15,7 +9,6 @@ namespace SampleWorkerRole
     {
         public override void Run()
         {
-            // This is a sample worker implementation. Replace with your logic.
             Trace.WriteLine("SampleWorkerRole entry point called", "Information");
 
             while (true)
@@ -27,11 +20,7 @@ namespace SampleWorkerRole
 
         public override bool OnStart()
         {
-            // Set the maximum number of concurrent connections 
             ServicePointManager.DefaultConnectionLimit = 12;
-
-            // For information on handling configuration changes
-            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
 
             return base.OnStart();
         }
